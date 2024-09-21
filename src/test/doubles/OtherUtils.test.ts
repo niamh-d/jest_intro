5b0f6535dbae92a88ca588825a5f68e27c9173f0
Niamh <niamh.linguist@gmail.com>
2024-09-21 14:57:48 +0300

    self-made mock

--- src/test/doubles/OtherUtils.test.ts
+++ src/test/doubles/OtherUtils.test.ts
@@ -1,6 +1,48 @@
-import { calculateComplexity } from "../../app/doubles/OtherUtils";
+import {
+  calculateComplexity,
+  toUpperCaseWithCb,
+} from "../../app/doubles/OtherUtils";
 
 describe("OtherUtils", () => {
+  describe("Tracking callbacks", () => {
+    let cbArgs = [];
+    let timesCalled = 0;
+
+    function callBackMock(arg: string) {
+      cbArgs.push(arg);
+      timesCalled++;
+    }
+
+    afterEach(() => {
+      cbArgs = [];
+      timesCalled = 0;
+    });
+
+    it("calls callback for invalid argument – track calls", () => {
+      const actual = toUpperCaseWithCb("", callBackMock);
+      expect(actual).toBeUndefined();
+      expect(cbArgs).toContain("Invalid argument");
+      expect(timesCalled).toBe(1);
+    });
+
+    it("calls callback for valid argument – track calls", () => {
+      const actual = toUpperCaseWithCb("abc", callBackMock);
+      expect(actual).toBe("ABC");
+      expect(cbArgs).toContain("called function with abc");
+      expect(timesCalled).toBe(1);
+    });
+  });
+
+  it("toUpperCase – calls callback for invalid argument", () => {
+    const actual = toUpperCaseWithCb("", () => {});
+    expect(actual).toBeUndefined();
+  });
+
+  it("toUpperCase – calls callback for valid argument", () => {
+    const actual = toUpperCaseWithCb("abc", () => {});
+    expect(actual).toBe("ABC");
+  });
+
   it("should calculate complexity", () => {
     const someInfo = {
       length: 5,
